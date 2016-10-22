e33aa6c3b62393c8775798bfcee5ee48b9058b6c
Alexandru Horeanu <horeanu.alexandru@gmail.com>
2016-10-22 10:12:48 +0300

    some configs

--- config/app.php
+++ config/app.php
@@ -163,6 +163,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Dingo\Api\Provider\LaravelServiceProvider::class,
+
         /*
          * Package Service Providers...
          */

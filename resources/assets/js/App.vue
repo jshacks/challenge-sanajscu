3c46a7b1747e93f6b69e40d094db55f96c684851
Daniel Nitu <nitu.contact@yahoo.com>
2016-10-23 11:30:59 +0300

    add ro map

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -6,6 +6,7 @@
 <script>
 
 require("./leaflet.js");
+require("./romania.js");
 
 var map;
 var ajaxRequest;
@@ -13,14 +14,16 @@ var plotlist;
 var plotlayers=[];
 
 function initmap() {
+
 	// set up the map
 	map = new L.Map('map');
 
 	// create the tile layer with correct attribution
 	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {maxZoom: 8, attribution: osmAttrib});
+	var osm = new L.TileLayer(osmUrl, {maxZoom: 10, minZoom: 4, attribution: osmAttrib});
 
+	L.geoJson(dataRo).addTo(map);
 	// start the map in Romania
 	map.setView(new L.LatLng(45.94, 24.97),7);
 	map.addLayer(osm);
